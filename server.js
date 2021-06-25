4b9f20cbc2a43804faecd41d737d0a52ace73719
surendar <gurusurendar99@gmail.com>
2021-06-25 22:28:25 +0530

    newm4

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(exp.static(path.join(__dirname,"./dist/practice")));
 
 
 
-//app.listen(process.env.PORT||8080)
-app.listen(3400,()=>console.log("server is on ",3400));
+app.listen(process.env.PORT||8080)
+//app.listen(3400,()=>console.log("server is on ",3400));
 
 
