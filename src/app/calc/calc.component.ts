0e0f036c6cd1841d5e744c3aac5a547308f82c6f
surendar <gurusurendar99@gmail.com>
2021-06-26 00:50:51 +0530

    newm5

--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -18,16 +18,32 @@ export class CalcComponent implements OnInit {
   ext;
   int;
   count1;
+  two2;
   newcount;
   st = false;
-  constructor() { }
+  st1 = false;
+  int1: any;
+  amt1: any;
+  diff1: number;
+  int2: number;
+  amt2: any;
+  newcount1: any;
+  st2 = false;
+  constructor() {}
 
   ngOnInit(): void {
+    
+  }
   
+  changed(){
+    console.log(this.st2);
+    this.st2 = true;
+    console.log("hi")
   }
   onSubmit(ref)
   {
     this.st = true;
+    this.st1 = true;
     console.log(ref.value)
     this.date = moment(new Date(ref.value.date1));
     console.log((this.date));
@@ -38,6 +54,7 @@ export class CalcComponent implements OnInit {
     this.newcount = this.current.diff(this.date, 'months');
     console.log(`${this.newcount} is the months diff`);
     this.count=(ref.value.amount);
+    console.log(this.st1);
     //this.count=this.current - this.date;
     //console.log(this.count)
     // this.month = (Math.floor(((this.current)-(this.date))/(this.days)));
@@ -53,14 +70,29 @@ export class CalcComponent implements OnInit {
     }
     else if(this.newcount>12)
     {
+      this.int1 = (ref.value.amount)*0.02*this.newcount;
+      this.amt1 = (ref.value.amount)+this.int1;
+      console.log(this.amt1);
+
+      this.newcount1 = this.newcount + 1;
+
+      this.int2 = (ref.value.amount)*0.01*(this.newcount1);
+      this.amt2 = (ref.value.amount)+this.int2;
+      console.log(this.amt2);
+
+
+     
+
       this.ext = this.newcount-12;
      
       console.log("newcount",this.ext);
       this.int = ((ref.value.amount)*0.025*this.ext)+((ref.value.amount)*0.02*12);
       this.amt = (ref.value.amount)+this.int;
       console.log( Math.ceil(this.amt));
+      this.diff1 = this.amt - this.amt1;
     }
 
   }
+
 
 }
